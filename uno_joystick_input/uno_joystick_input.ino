dc4afd9baefefa714c32b094d997129207c90a95
Joey Jacobus <joeyjacobus@comcast.net>
2018-04-22 12:48:16 -0600

    Got it to fly, about to add yaw correction

--- uno_joystick_input/uno_joystick_input.ino
+++ uno_joystick_input/uno_joystick_input.ino
@@ -26,9 +26,33 @@
  http://arduino.cc/en/Tutorial/AnalogInput
  
  */
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+
+// For the breakout, you can use any 2 or 3 pins
+// These pins will also work for the 1.8" TFT shield
+#define TFT_CS     10
+#define TFT_RST    9  // you can also connect this to the Arduino reset
+                      // in which case, set this #define pin to -1!
+#define TFT_DC     7
+// Option 1 (recommended): must use the hardware SPI pins
+// (for UNO thats sclk = 13 and sid = 11) and pin 10 must be
+// an output. This is much faster - also required if you want
+// to use the microSD card (see the image drawing example)
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+ 
 #define ESP_ENABLE 8
 #define ESP_RST 7
 #define LS_CLICK_PIN 2
+
+
+#define SPEED_MIN 1000
+#define MIN_THRUST 1150
+#define TAKEOFF_THRUST 1350
+#define MAX_CONTROLLER_THRUST 1425                        // The maximum value the controller can set the base thrust
+
+
 
 int sensorPinLX = A5;
 int sensorPinLY = A4;    // select the input pin for the potentiometer
@@ -50,6 +74,42 @@ void leftStickClicked(void){
   WAITFORSTART= true;
 }
 
+
+void testdrawtext(char *text, uint16_t color) {
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+}
+
+void tftWriteVals( int lx, int ly, int rx, int ry) {
+  tft.setTextWrap(true);
+  //tft.fillScreen(ST7735_BLACK);
+  
+  /* clear the screen */
+  
+  //tft.fillRect(0, 10, tft.width(), 60,ST7735_BLACK);
+  
+  tft.setCursor(0, 10);
+  tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+  tft.setTextSize(1);
+
+  tft.print("lx:");
+  tft.print(lx);
+  tft.println(" ");
+  tft.print("ly:");
+  tft.print(ly); 
+  tft.println(" ");
+  tft.print("rx:");
+  tft.print(rx);
+  tft.println(" ");
+  tft.print("ry:");
+  tft.print(ry);
+  tft.println(" ");
+}
+
+
+
 void setup() {
   // declare the ledPin as an OUTPUT:
   pinMode(ledPin, OUTPUT);  
@@ -60,21 +120,26 @@ void setup() {
   Serial.begin(115200);
   pinMode(LS_CLICK_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(LS_CLICK_PIN), leftStickClicked, FALLING);
-  //Serial2.begin(115200);
+
+  // Use this initializer if you're using a 1.8" TFT
+  tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
+  tft.fillScreen(ST7735_BLACK);
+  testdrawtext("Click the left stick to continue!", ST7735_WHITE);
+  tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+  tft.setTextSize(1);
   while(!WAITFORSTART){
     delay(100);
   }
+  tft.fillScreen(ST7735_BLACK);
+
 }
 
-void loop() {
-  // read the value from the sensor:
-  sensorValueLX = analogRead(sensorPinLX); 
-  sensorValueLY = analogRead(sensorPinLY); 
-  sensorValueRX = analogRead(sensorPinRX); 
-  sensorValueRY = analogRead(sensorPinRY);   
-    
-  // turn the ledPin on
-  digitalWrite(ledPin, HIGH);  
+/*
+ * Sends the longer format string
+ * "LX=512 LY=234 RX=232 RY=232 CK=xxx" 
+ */
+void sendJoystickVals(void){
+  int checksum = (sensorValueLX + sensorValueLY + sensorValueRX + sensorValueRY) % 256;
   // stop the program for <sensorValue> milliseconds:
   Serial.print("LX=");
   Serial.print(sensorValueLX); 
@@ -87,22 +152,61 @@ void loop() {
   Serial.print(" ");  
   Serial.print("RY=");        
   Serial.print(sensorValueRY); 
+  Serial.print(" ");  
+  Serial.print("CK=");        
+  Serial.print(checksum);  
   Serial.print("\n");
-//  Serial2.print("LX=");
-//  Serial2.print(sensorValueLX); 
-//  Serial2.print(" ");          
-//  Serial2.print("LY=");
-//  Serial2.print(sensorValueLY); 
-//  Serial2.print(" ");
-//  Serial2.print("RX=");
-//  Serial2.print(sensorValueRX); 
-//  Serial2.print(" ");  
-//  Serial2.print("RY=");        
-//  Serial2.print(sensorValueRY); 
-//  Serial2.print("\n");
+}
+
+
+void sendBaseThrust(void){
+  int thrust;
+  int base1 = sensorValueLY - 512;
+  int base2 = sensorValueRY - 512;
+  if (base1 < 30 && base1 > -30){
+    base1 = 0;
+  }    
+  if (base2 < 30 && base2 > -30){
+    base2 = 0;
+  }    
+  
+  if (base1 == 0 && base2 == 0){
+    thrust = SPEED_MIN;
+  }
+  else {
+    double ratio1 = base1 / 512.0;
+    double ratio2 = base2 / 512.0;
+    thrust = MIN_THRUST + (TAKEOFF_THRUST - MIN_THRUST) * ratio1;
+    thrust +=  (MAX_CONTROLLER_THRUST - TAKEOFF_THRUST) * ratio2;
+  }
+  Serial.print(thrust);
+  Serial.print(" ");
+  Serial.print(thrust);
+  Serial.print("\n");
+  tft.setCursor(10, 70);
+  tft.print("Base:");
+  tft.print(thrust);
+
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValueLX = analogRead(sensorPinLX); 
+  sensorValueLY = analogRead(sensorPinLY); 
+  sensorValueRX = analogRead(sensorPinRX); 
+  sensorValueRY = analogRead(sensorPinRY);   
+  // turn the ledPin on
+  digitalWrite(ledPin, HIGH);  
+
+  
+  sendBaseThrust();
+  //sendJoystickVals();
+ 
+  
+  //tftWriteVals(sensorValueLX, sensorValueLY, sensorValueRX, sensorValueRY);
         
   // turn the ledPin off:        
   digitalWrite(ledPin, LOW);   
   // stop the program for for <sensorValue> milliseconds:
-  delay(150);                  
+  delay(10);                  
 }
